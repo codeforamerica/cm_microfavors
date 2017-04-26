8cff807656b9a4fedb30ca1421084ca18d7631a4
Paras Sanghavi <paras@codeforamerica.org>
2017-04-26 14:25:14 -0700

    rename

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: 'jobseekers#index'
-  get '/resume-feedback', to: 'jobseekers#resume-feedback'
-  get '/common-questions', to: 'jobseekers#common-questions'
-  get '/am-i-qualified', to: 'jobseekers#am-i-qualified'
+  get '/resume_feedback', to: 'jobseekers#resume_feedback'
+  get '/common_questions', to: 'jobseekers#common_questions'
+  get '/am_i_qualified', to: 'jobseekers#am_i_qualified'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
