6edec43fee741ee7e6bd0da865c6ce8906cd4f5a
Paras Sanghavi <paras@codeforamerica.org>
2017-04-26 14:37:37 -0700

    combine all questions

--- app/controllers/jobseekers_controller.rb
+++ app/controllers/jobseekers_controller.rb
@@ -1,2 +1,19 @@
 class JobseekersController < ApplicationController
+  def resume_feedback
+    @header = 'Get resume feedback'
+
+    render :ask
+  end
+
+  def common_questions
+    @header = 'Answer common questions'
+
+    render :ask
+  end
+
+  def am_i_qualified
+    @header = 'Find out if you are qualified'
+
+    render :ask
+  end
 end
