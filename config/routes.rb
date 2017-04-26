e729285a017b53a5fcb807e5c8861c611361a784
Paras Sanghavi <paras@codeforamerica.org>
2017-04-26 11:12:19 -0700

    add templates for other two tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'jobseekers#index'
   get '/resume-feedback', to: 'jobseekers#resume-feedback'
+  get '/common-questions', to: 'jobseekers#common-questions'
+  get '/am-i-qualified', to: 'jobseekers#am-i-qualified'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/features/jobseeker_spec.rb
+++ spec/features/jobseeker_spec.rb
@@ -1,14 +1,21 @@
-feature 'Home page' do
-  scenario 'visit the home page' do
+feature 'Jobseeker interactions' do
+  before do
     visit root_path
     expect(page).to have_content 'Welcome'
   end
 
   scenario 'get resume feedback' do
-    visit root_path
-    expect(page).to have_content 'Welcome'
-
     click_on 'Resume feedback'
     expect(page).to have_content 'Get resume feedback'
+  end
+
+  scenario 'answer common questions' do
+    click_on 'Common questions'
+    expect(page).to have_content 'Answer common questions'
+  end
+
+  scenario 'am i qualified' do
+    click_on 'Am I qualified for this job?'
+    expect(page).to have_content 'Find out if you are qualified'
   end
 end
