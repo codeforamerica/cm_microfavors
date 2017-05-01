44fd35f2653b14e628c5278606890e063e669cb4
Paras Sanghavi <paras@codeforamerica.org>
2017-05-01 14:10:29 -0700

    cover letter content

--- app/controllers/jobseekers_controller.rb
+++ app/controllers/jobseekers_controller.rb
@@ -1,10 +1,4 @@
 class JobseekersController < ApplicationController
-  def cover_letter_feedback
-    @header = 'Cover Letter Help'
-
-    ask
-  end
-
   def am_i_qualified
     @header = 'Find out if you are qualified'
 
